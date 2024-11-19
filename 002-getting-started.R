401e4c20a9b1ca790f9ed9dec9b9f55c179a8c2c
valatwork <valfuindor@outlook.com>
2024-11-19 17:34:25 +0100

    Empty commit

---
+++ 002-getting-started.R
@@ -0,0 +1,25 @@
+# math calculations
+
+# saving the output in a variable
+result <- (2+4-1)*10
+result
+assign("result2",(2+4-1)*10)
+result2
+result == result2
+
+# assign values to multiple variables
+
+calculation <- result <- (2+4-1)*10
+calculation == result
+
+# built in functions examples
+abs(-4)
+factorial(4)
+
+#logical operators
+1 == 1
+2 > 5
+6 < 4
+2 > 5 & 5 > 1
+2 > 5 | 5 > 1
+result != 55
