26a583014c22105acb562c3790d90e365bc0d8bc
valatwork <valfuindor@outlook.com>
2024-11-20 17:07:05 +0100

    Empty commit

---
+++ 003-data-types.R
@@ -0,0 +1,25 @@
+# character variable
+product <- 'cookie'
+product
+typeof(product)
+
+# double and integer variables
+quantity <- 2
+quantity
+price <- 3
+price
+typeof(quantity)
+typeof(price)
+
+quantity_integer <- 2L # L stores the variable as an integer
+quantity_integer
+typeof(quantity_integer)
+
+
+# logical variable
+discount1 <- FALSE
+discount2 <- F
+
+is.numeric(quantity)
+is.integer(quantity)
+is.logical(discount1)
